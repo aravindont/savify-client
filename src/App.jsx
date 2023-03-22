bac1916f7c52b9b6471b07e5c1c187589ccf4496
Aravind104 <ontagodiaravind@gmail.com>
2023-03-22 15:14:31 +0530

    fix page not found error when deployed to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Topbar } from "./components/Topbar";
 
 import { Navigate, Route, Routes } from "react-router-dom";
+import { AddContact } from "./components/AddContact";
 function App() {
   return (
     <>
       <Topbar />
       <Routes>
-        <Route />
+        {/* <Route path="/" element={<Navigate to={"/contacts/list"} />} />
+        <Route path="/contacts/list" element={<Conta />} /> */}
+        <Route path="/contacts/add" element={<AddContact />} />
       </Routes>
     </>
   );
