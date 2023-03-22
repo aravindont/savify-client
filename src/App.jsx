60f51513c2064ab4de1e840d8fa3290cd1ea3cf8
Aravind104 <ontagodiaravind@gmail.com>
2023-03-22 16:45:04 +0530

    add route to access the contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Topbar } from "./components/Topbar";
-
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AddContact } from "./components/AddContact";
+import { ContactList } from "./components/ContactList";
 function App() {
   return (
     <>
       <Topbar />
       <Routes>
-        {/* <Route path="/" element={<Navigate to={"/contacts/list"} />} />
-        <Route path="/contacts/list" element={<Conta />} /> */}
+        <Route path="/" element={<Navigate to={"/contacts/list"} />} />
+        <Route path="/contacts/list" element={<ContactList />} />
         <Route path="/contacts/add" element={<AddContact />} />
       </Routes>
     </>
